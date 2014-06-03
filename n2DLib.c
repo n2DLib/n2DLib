7914a490f1e12b487221cce442d364cc3ed8b9ad
matrefeytontias <mattias@refeyton.fr>
2014-06-03 08:50:14 +0200

    Fixed name

---
+++ n2DLib.c
@@ -0,0 +1,172 @@
+#include "n2DLib.h"
+
+// Double-buffering
+
+int currentBuf = 0;
+void *bufferBackup;
+unsigned short *doubleBuf[2];
+
+int initDoubleBuff()
+{
+	bufferBackup = (void*)SCREEN_BASE_ADDRESS;
+	// Sets the monochrome screen to 16-bits
+	if(!has_colors)
+		*(int32_t*)0xC000001C = (*((int32_t*)0xC000001C) & ~0b1110) | 0b1000;
+	doubleBuf[0] = (unsigned short*)malloc(BUFF_BYTES_SIZE);
+	doubleBuf[1] = (unsigned short*)malloc(BUFF_BYTES_SIZE);
+	
+	return !doubleBuf[0] | !doubleBuf[1];
+}
+
+void switchBuffersRestore()
+{
+	memcpy(doubleBuf[currentBuf ^ 1], BUFF_BASE_ADDRESS, BUFF_BYTES_SIZE);
+	*(unsigned short **)0xC0000010 = BUFF_BASE_ADDRESS;
+	currentBuf ^= 1;
+}
+
+void switchBuffers()
+{
+	*(unsigned short **)0xC0000010 = BUFF_BASE_ADDRESS;
+	currentBuf ^= 1;
+}
+
+void deinitDoubleBuff()
+{
+	// Sets the monochrome screen back to 4-bits
+	if(!has_colors)
+		*((int32_t*)0xC000001C) = (*((int32_t*)0xC000001C) & ~0b1110) | 0b0100;
+	*(void**)0xC0000010 = bufferBackup;
+	free(doubleBuf[0]);
+	free(doubleBuf[1]);
+}
+
+// Maths
+Fixed fixcos(Fixed angle)
+{
+	static Fixed cosLUT[] = { 256, 255, 255, 255, 254, 254, 253, 252, 251, 249, 248, 246, 244, 243, 241, 238, 236, 234, 231, 228, 225, 222, 219, 216, 212, 209, 205, 201, 197, 193, 189, 185, 181, 176, 171, 167, 162, 157, 152, 147, 142, 136, 131, 126, 120, 115, 109, 103, 97, 92, 86, 80, 74, 68, 62, 56, 49, 43, 37, 31, 25, 18, 12, 6, 0, -6, -12, -18, -25, -31, -37, -43, -49, -56, -62, -68, -74, -80, -86, -92, -97, -103, -109, -115, -120, -126, -131, -136, -142, -147, -152, -157, -162, -167, -171, -176, -181, -185, -189, -193, -197, -201, -205, -209, -212, -216, -219, -222, -225, -228, -231, -234, -236, -238, -241, -243, -244, -246, -248, -249, -251, -252, -253, -254, -254, -255, -255, -255, -256, -255, -255, -255, -254, -254, -253, -252, -251, -249, -248, -246, -244, -243, -241, -238, -236, -234, -231, -228, -225, -222, -219, -216, -212, -209, -205, -201, -197, -193, -189, -185, -181, -176, -171, -167, -162, -157, -152, -147, -142, -136, -131, -126, -120, -115, -109, -103, -97, -92, -86, -80, -74, -68, -62, -56, -49, -43, -37, -31, -25, -18, -12, -6, 0, 6, 12, 18, 25, 31, 37, 43, 49, 56, 62, 68, 74, 80, 86, 92, 97, 103, 109, 115, 120, 126, 131, 136, 142, 147, 152, 157, 162, 167, 171, 176, 181, 185, 189, 193, 197, 201, 205, 209, 212, 216, 219, 222, 225, 228, 231, 234, 236, 238, 241, 243, 244, 246, 248, 249, 251, 252, 253, 254, 254, 255, 255, 255 };
+	return cosLUT[angle & 0xff];
+}
+
+void rotate(int x, int y, Fixed ca, Fixed sa, Rect* out)
+{
+	out->x = fixtoi(fixmul(itofix(x), ca) + fixmul(itofix(y), sa));
+	out->y = fixtoi(fixmul(itofix(x), -sa) + fixmul(itofix(y), ca));
+}
+
+// Graphics
+
+inline void clearBuffer()
+{
+	memset(BUFF_BASE_ADDRESS, 0, BUFF_BYTES_SIZE);
+}
+
+inline unsigned short getPixel(unsigned short *src, int x, int y)
+{
+	if(x >= 0 && x < src[0] && y >= 0 && y < src[1])
+		return src[x + y * src[0] + 3];
+	else
+		return src[2];
+}
+
+inline void setPixel(int x, int y, unsigned short c)
+{
+	if(x >= 0 && x < 320 && y >= 0 && y < 240)
+	{
+		if(has_colors)
+			*((unsigned short*)BUFF_BASE_ADDRESS + x + (y << 8) + (y << 6)) = c;
+		else
+		{
+			c = ~c;
+			*((unsigned short*)BUFF_BASE_ADDRESS + x + (y << 8) + (y << 6)) = ((c >> 11) + ((c & 0x07c0) >> 6) + (c & 0x1f)) & 0xffff;
+		}
+	}
+}
+
+inline void setPixelRGB(int x, int y, unsigned char r, unsigned char g, unsigned char b)
+{
+	if(x >= 0 && x < 320 && y >= 0 && y < 240)
+	{
+		if(has_colors)
+			*((unsigned short*)BUFF_BASE_ADDRESS + x + (y << 8) + (y << 6)) = ((r >> 3) << 11) | ((g >> 2) << 5) | (b >> 3);
+		else
+			*((unsigned short*)BUFF_BASE_ADDRESS + x + (y << 8) + (y << 6)) = ~(r + g + b) & 0xffff;
+	}
+}
+
+void drawSprite(unsigned short *src, int _x, int _y)
+{
+	int x, y, w = src[0] + _x, h = src[1] + _y, c = 3;
+	for(y = _y; y < h; y++)
+	{
+		for(x = _x; x < w; x++, c++)
+		{
+			if(src[c] != src[2])
+				setPixel(x, y, src[c]);
+		}
+	}
+}
+
+void drawSpritePart(unsigned short *src, int _x, int _y, Rect part)
+{
+	unsigned short c;
+	int x, y = _y, w = part.w + _x, h = part.h + _y, z = part.x, t = part.y;
+	for(y = _y; y < h; y++, t++)
+	{
+		for(x = _x, z = part.x; x < w; x++, z++)
+		{
+			c = getPixel(src, z, t);
+			if(c != src[2])
+				setPixel(x, y, c);
+		}
+	}
+}
+
+void drawSpriteRotated(unsigned short* source, Rect sr, Fixed angle)
+{
+	Rect upleft, upright, downleft, downright;
+	Rect fr;
+	unsigned short currentPixel;
+	Fixed dX = fixcos(angle), dY = fixsin(angle);
+	
+	rotate(-source[0] / 2, -source[1] / 2, dX, dY, &upleft);
+	rotate(source[0] / 2, -source[1] / 2, dX, dY, &upright);
+	rotate(-source[0] / 2, source[1] / 2, dX, dY, &downleft);
+	rotate(source[0] / 2, source[1] / 2, dX, dY, &downright);
+	
+	fr.x = min(min(min(upleft.x, upright.x), downleft.x), downright.x) + sr.x;
+	fr.y = min(min(min(upleft.y, upright.y), downleft.y), downright.y) + sr.y;
+	fr.w = max(max(max(upleft.x, upright.x), downleft.x), downright.x) + sr.x;
+	fr.h = max(max(max(upleft.y, upright.y), downleft.y), downright.y) + sr.y;
+	
+	Rect cp, lsp, cdrp;
+	
+	lsp.x = fixmul(itofix(fr.x - sr.x), dX) + fixmul(itofix(fr.y - sr.y), -dY);
+	lsp.y = fixmul(itofix(fr.x - sr.x), dY) + fixmul(itofix(fr.y - sr.y), dX);
+	
+	for(cp.y = fr.y; cp.y < fr.h; cp.y++)
+	{
+		cdrp.x = lsp.x;
+		cdrp.y = lsp.y;
+		
+		for(cp.x = fr.x; cp.x < fr.w; cp.x++)
+		{
+			if(cp.x >= 0 && cp.x < 320 && cp.y >= 0 && cp.y < 240)
+			{
+				if(abs(fixtoi(cdrp.x)) < source[0] / 2 && abs(fixtoi(cdrp.y)) < source[1] / 2)
+				{
+					currentPixel = getPixel(source, fixtoi(cdrp.x) + source[0] / 2, fixtoi(cdrp.y) + source[1] / 2);
+					if(currentPixel != source[2])
+					{
+						setPixel(cp.x, cp.y, currentPixel);
+					}
+				}
+			}
+			cdrp.x += dX;
+			cdrp.y += dY;
+		}
+		
+		lsp.x -= dY;
+		lsp.y += dX;
+	}
+}
---
+++ n2DLib.h
@@ -0,0 +1,42 @@
+#ifndef INCLUDE_GRAFX
+#define INCLUDE_GRAFX
+
+#include <os.h>
+
+typedef int Fixed;
+typedef struct
+{
+	int x;
+	int y;
+	int h;
+	int w;
+} Rect;
+
+#define itofix(x) ((x) << 8)
+#define fixtoi(x) ((x) >> 8)
+#define fixmul(x, y) ((x)*  (y) >> 8)
+#define fixdiv(x, y) (((x) << 8) / (y))
+
+#define fixsin(x) fixcos((x) - 64)
+
+extern Fixed fixcos(Fixed angle);
+extern void rotate(int x, int y, Fixed ca, Fixed sa, Rect* out);
+
+extern int currentBuf;
+extern unsigned short *doubleBuf[2];
+
+extern int initDoubleBuff();
+extern void switchBuffers();
+extern void switchBuffersRestore();
+extern void deinitDoubleBuff();
+extern inline void clearBuffer();
+extern inline void setPixel(int, int, unsigned short);
+extern inline void setPixelRGB(int, int, unsigned char, unsigned char, unsigned char);
+extern void drawSprite(unsigned short*, int, int);
+extern void drawSpritePart(unsigned short*, int, int, Rect);
+extern void drawSpriteRotated(unsigned short*, Rect, Fixed);
+
+#define BUFF_BASE_ADDRESS doubleBuf[currentBuf]
+#define BUFF_BYTES_SIZE (320*240*2)
+
+#endif
